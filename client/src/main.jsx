2834380212620dd85b7e1ef6e5dc5078003e612e
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-21 23:33:57 +0300

    chore: Update main.jsx to include BrowserRouter component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
-import App from './App.jsx'
+
 import './index.css'
+import App from './App.jsx'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
